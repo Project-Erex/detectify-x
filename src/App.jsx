578636955dc659a590830b93a719d2ced1cf6cd3
devmithun89 <mithungorai662@gmail.com>
2023-09-24 16:04:52 +0530

    implement charcount & wordcount

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import {useState} from "react";
-
-// import Sidebar from './Components/Sidebar/Sidebar'
-
 import Maincontent from "./Components/Main/Maincontent";
 function App() {
   const [count, setCount] = useState(0);
